cec3acec840b77131ecaba17a961f18d8d60a9ad
michaelsd28 <santana11281@yahoo.com>
2021-07-26 22:41:17 -0400

    c

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -4,22 +4,19 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div
-    animate={{marginTop:200,rotateZ:180, opacity:200}}
-    >
-    <div className="home container">
-      <motion.h2 animate={{ color: '#a20fc1', fontSize: 50,  x: 100, y: -100 }}>
-        <h2>Welcome to Pizza Joint</h2>
-      </motion.h2>
-      <Link to="/base">
-
-        <motion.button
-        animate={{scale:1.3}}
+    <motion.div animate={{ marginTop: 200, rotateZ: 180, opacity: 200 }}>
+      <div className="home container">
+        <motion.h2
+          animate={{ color: "#a20fc1", fontSize: 50, x: 100, y: -100 }}
         >
-        <button>Create Your Pizza</button>
-        </motion.button>
-      </Link>
-    </div>
+          <h2>Welcome to Pizza Joint</h2>
+        </motion.h2>
+        <Link to="/base">
+          <motion.button animate={{ scale: 1.3 }}>
+            <button>Create Your Pizza</button>
+          </motion.button>
+        </Link>
+      </div>
     </motion.div>
   );
 };
