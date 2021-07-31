7ff83a2012620f326eeac685f0338f54c700e2e4
michaelsd28 <santana11281@yahoo.com>
2021-07-31 01:57:40 -0400

    c

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -1,36 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { motion,AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+
+const backdrop = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 
 
-const backdrop ={
-    hidden:{opacity:0},
-visible:{opacity:1}
+const modal = {
+  
+  hidden: {
+    y:"400px",
+    zIndex:-100,
+    opacity: 0,
+    scale:0.2,
+
+  },
+  visible: {
+    zIndex:2,
+    scale:1.1,
+opacity:1,
+transition:{delay:0.5}
 
 
-}
+  },
+};
 
-
-function Modal({ showModal,setShowModal }) {
+function Modal({ showModal, setShowModal }) {
   return (
     <AnimatePresence exitBeforeEnter>
-      {showModal && <motion.div className="backdrop"
+      {showModal && (
+        <motion.div
 
-      variants={backdrop}
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      >
-          <motion.div className="modal"> 
+          className="backdrop"
+          variants={backdrop}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <motion.div  className="modal"
+          variants={modal}
+    
 
-          <p>want to make another order</p>
-          <Link to="/">
-            <button>start again</button>
-          </Link>
+          >
+            <p>want to make another order</p>
+            <Link to="/">
+              <button
+              onClick={()=>{
 
+                  setShowModal(false)
+
+              }}
+              >start again</button>
+            </Link>
           </motion.div>
-          </motion.div>}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
